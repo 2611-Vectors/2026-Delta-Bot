52423760852835e366d5148bc364f036e1777835
logan <logangale2009@gmail.com>
2026-07-26 10:10:54 -0400

    Brake mode again

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -23,7 +23,7 @@ public class Pivot extends SubsystemBase {
 
   public Pivot() {
     pivotEncoder.setInverted(false);
-    pivotMotor.setBrakeMode(NeutralModeValue.Coast);
+    pivotMotor.setBrakeMode(NeutralModeValue.Brake);
     pivotMotor.setInverted(InvertedValue.Clockwise_Positive);
   }
 
